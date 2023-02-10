18d3795d5c94c3e03e6ce89d48fb7a15edc2692c
R1k0523 <isergey555345@gmail.com>
2023-02-10 17:31:38 +0300

    notification rework

--- src/main/kotlin/ru/boringowl/common/domain/model/Notification.kt
+++ src/main/kotlin/ru/boringowl/common/domain/model/Notification.kt
@@ -3,22 +3,31 @@ package ru.boringowl.common.domain.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class Notification {
-    abstract val receiverAddress: String
-    abstract val receiverName: String
-}
-
-@Serializable
 class CodeNotification(
-    override val receiverAddress: String,
-    override val receiverName: String,
-    val type: NotificationType,
+    val receiverAddress: String,
+    val receiverName: String,
+    val type: CodeNotificationType,
     val code: String
-): Notification()
+)
 
 @Serializable
-enum class NotificationType {
+class CommonNotification(
+    val to: String,
+    val by: String,
+    val at: Long,
+    val where: String,
+    val body: String,
+    val type: CommonNotificationType,
+)
+
+@Serializable
+enum class CodeNotificationType {
     RESET_PASSWORD,
     VERIFY_EMAIL,
     SIGN_UP
+}
+
+@Serializable
+enum class CommonNotificationType {
+    SYSTEM, USER, COMMUNITY
 }
